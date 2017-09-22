b37cf6ee78b7700039839dda005e6138a7e81a90
Benjamin Stephens <bs@NCC1776.local>
2017-09-22 12:31:02 -0700

    Further enhances visual style

--- bundle.js
+++ bundle.js
@@ -450,7 +450,7 @@ module.exports = function() {
   d3.select("#startButton").on('click', () => {
     let boardSize = d3.select("#num").node().value;
     let newGame = new NQueenVis(boardSize);
-    debugger;
+    // debugger;
     newGame.play();
   });
 }
